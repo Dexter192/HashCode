67fb5bd5df423cba7ed4b7c50552a9f2b08b1d8a
jm-glowienke <jmglowienke@gmail.com>
2021-02-25 21:01:36 +0100

    pipeline for all files done

--- a.py
+++ a.py
@@ -1,23 +1,24 @@
 from hashcode_parser import *
 
-simulation_info, cars, streets = load_file('a.txt')
+for name in ["a","b","c","d","e","f"]:
+    simulation_info, cars, streets = load_file(name+'.txt')
 
-it = 0
-intersections = dict()
-for it in range(0,simulation_info.total_intersections):
-    intersection = Intersection(it)
-    intersections[it] = intersection
-    it += 1
+    it = 0
+    intersections = dict()
+    for it in range(0,simulation_info.total_intersections):
+        intersection = Intersection(it)
+        intersections[it] = intersection
+        it += 1
 
-#print(list(intersections.values()))
-#{0 -> Intersection with id 0, 1 -> Intersection with id 1}
-for street in streets:
-    intersections[street.end].add_street(street)
+    #print(list(intersections.values()))
+    #{0 -> Intersection with id 0, 1 -> Intersection with id 1}
+    for street in streets:
+        intersections[street.end].add_street(street)
 
 
-#ALGORITHM 1 REALLY BAD
-for intersection in intersections.values():
-    intersection.cycle = [(s.name ,1) for s in intersection.streets]
-    print(intersection.cycle)
+    #ALGORITHM 1 REALLY BAD
+    for intersection in intersections.values():
+        intersection.cycle = [(s.name ,1) for s in intersection.streets]
+        print(intersection.cycle)
 
-write_output('a.out', intersections.values())
+    write_output(name+'.out', intersections.values())
