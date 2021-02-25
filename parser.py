2a70a1bf9b2d6274f1d569355bcd90c1ca931d2e
jm-glowienke <jmglowienke@gmail.com>
2021-02-25 19:51:32 +0100

    loading done

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import numpy as np
 
-class simulation:
+class Simulation:
     def __init__(self, duration, total_intersections, total_streets, total_cars, bonus_points):
         self.duration = duration
         self.total_intersections = total_intersections
@@ -10,23 +10,23 @@ class simulation:
         self.total_cars = total_cars
         self.bonus_points = bonus_points
 
-class car:
+class Car:
     def __init__(self, num_streets, route):
         self.num_streets = num_streets
         self.route = route
-        self.current_street = route[0]
+        #self.current_street = route[0]
         #Gets the current position in the current street
-        self.current_position = route[0].travel_time
+        #self.current_position = route[0].travel_time
 
-class street:
-    def __init__(self,start,end,name,travel_time):
+class Street:
+    def __init__(self, start,end,name,travel_time):
         self.start = start
         self.end = end
         self.name = name
         self.travel_time = travel_time
 
 
-class intersection:
+class Intersection:
     def __init__(self, id, number_streets, streets):
         self.number_streets = number_streets
         self.streets = streets
@@ -41,21 +41,21 @@ def load_file(name):
     with open('input/'+name) as infile:
         lines = [line.strip() for line in infile.readlines()]
     simulation_info = load_simulation_info(lines[0])
-    cars = load_cars(lines[1:simulation_info.total_streets])
-    streets = load_streets(lines[simulation_info.total_streets+1:-1])
+    streets = load_streets(lines[1:simulation_info.total_streets])
+    cars = load_cars(lines[simulation_info.total_streets+1:-1])
     return simulation_info, cars, streets
 
 def load_simulation_info(input_line):
     splitted = input_line.split(' ')
-    splitted = [[int(x) for x in list] for list in splitted]
-    return simulation( splitted[0], splitted[1], splitted[2], splitted[3], splitted[4])
+    splitted = [int(x) for x in splitted]
+    return Simulation(splitted[0], splitted[1], splitted[2], splitted[3], splitted[4])
 
 
 def load_cars(lines):
     list_cars=[]
     for line in lines:
-        attributes = [line.split(' ')]
-        car1 = car(self, int(attributes[0], attributes[1:-1]))
+        attributes = line.split(' ')
+        car1 = Car(int(attributes[0]), attributes[1:-1])
         list_cars.append(car1)
     return list_cars
 
@@ -63,11 +63,12 @@ def load_cars(lines):
 def load_streets(lines):
     list_streets = []
     for line in lines:
-        splitted = [line.split(' ')]
-        list_streets.append(street(int(splitted[0]),int(splitted[1]),splitted[2],int(splitted[3])))
+        splitted = line.split(' ')
+        street = Street(int(splitted[0]),int(splitted[1]),splitted[2],int(splitted[3]))
+        list_streets.append(street)
     return list_streets
 
 simulation_info, cars, streets = load_file('a.txt')
 print(simulation_info.duration)
-print(cars[0].route)
+print(cars[0].route[0])
 print(streets[0].name)
