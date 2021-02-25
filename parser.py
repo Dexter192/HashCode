5c09670c825ff21b78bba0f9a3604048fead6ad9
DanielKaestner <DanielKaestner@outlook.de>
2021-02-25 18:05:31 +0100

    Updated parser

---
+++ parser.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import os
+import numpy as np
+
+def load_file(name):
+    with open('input/'+name) as infile:
+        return [line.strip() for line in infile.readlines()]
+    return lines
+
+#the_file = load_file("/input/a_exa")
+
+def clean_file(the_file):
+    splitted = [file.split(' ') for file in the_file]
+    return splitted
+
+#cleaned_file = clean_file(the_file)
+
+def flat_list(cleaned_file):
+    flattened_list = []
+    for sublist in cleaned_file:
+        for item in sublist:
+            flattened_list.append(int(item))
+    return flattened_list
+
+
+#df = np.asarray(flat_list(cleaned_file))
+#print(df)
+
+def load_files():
+    names = os.listdir("./input/")
+    print(names)
+    inputs = []
+    for file_name in names:
+        input = load_file(file_name)
+        clean_input = clean_file(input)
+        inputs.append(clean_input)
+    return inputs
+
+input = load_files()
+print(input)
