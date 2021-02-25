86d70762ca3951e797556b35de2b1ce35eaa356c
jm-glowienke <jmglowienke@gmail.com>
2021-02-25 19:25:00 +0100

    added classes

--- parser.py
+++ parser.py
@@ -2,6 +2,41 @@ import pandas as pd
 import os
 import numpy as np
 
+class simulation:
+    def __init__(duration, num_intersections, total_streets, total_cars, bonus_points):
+        self.duration = duration
+        self.total_intersections = total_intersections
+        self.total_streets = total_streets
+        self.total_cars = total_cars
+        self.bonus_points = bonus_points
+
+class car:
+    def __init__(self, num_streets, route):
+        self.num_streets = num_streets
+        self.route = route
+        self.current_street = route[0]
+        #Gets the current position in the current street
+        self.current_position = route[0].travel_time
+
+class street:
+    def __init__(self,start,end,name,travel_time):
+        self.start = start
+        self.end = end
+        self.name = name
+        self.travel_time = travel_time
+
+
+class intersection:
+    def __init__(self, id, number_streets, streets):
+        self.number_streets = number_streets
+        self.streets = streets
+        self.green = np.zeros(len(simulation.duration))
+        self.id = id
+        #List of tuples (routename, seconds green)
+        self.cycle = []#[(routename, 2), (routename2, 3), (routename3, 1)]
+
+
+
 def load_file(name):
     with open('input/'+name) as infile:
         return [line.strip() for line in infile.readlines()]
